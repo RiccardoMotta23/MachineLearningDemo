50eac4d33f33e570400f7002286cc3af75160b9e
RiccardoMotta23 <riccardomotta23@gmail.com>
2025-02-26 09:34:30 +0100

    Created classification.py
    Created the script to show the RandomForest classification.

---
+++ classification.py
@@ -0,0 +1,16 @@
+# --- IMPORT SECTION ---
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier # to standardize (scale) the feature (the X)
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report # To evaluate the model
+import seaborn as sns
+# --- END OF IMPORT SECTION ---
+
+# --- MAIN CODE ---
+
+
+
+# --- END OF MAIN CODE ---
